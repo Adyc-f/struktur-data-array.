9d464bf898f4d55cf93c25e6354b9c025a52be72
Adyc-f <sudaniniwayan11@gmail.com>
2026-04-22 19:28:46 +0800

    feat: menambahkan perhitungan nilai tertinggi dan terendah

--- Soal_01.py
+++ Soal_01.py
@@ -9,3 +9,18 @@ for i in range(10):
     nilai.append(n)
 
 print("\nData nilai:", nilai)
+
+# mencari nilai tertinggi
+maks = nilai[0]
+for n in nilai:
+    if n > maks:
+        maks = n
+
+# mencari nilai terendah
+minim = nilai[0]
+for n in nilai:
+    if n < minim:
+        minim = n
+
+print("\nNilai tertinggi:", maks)
+print("Nilai terendah:", minim)
